0886c1c59417e7e7c1a71036f76ea5c68af7bf30
Naufal rizki rabbani <naufal.rabbani132@gmail.com>
2025-06-23 19:04:39 +0700

    Update Gallery.tsx
    ukuran

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -74,7 +74,7 @@ const Gallery: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="text-center mb-12">
-          <h1 className="text-5xl md:text-6xl font-dancing font-bold text-purple-800 mb-4">
+          <h1 className="text-2xl md:text-2xl font-dancing font-bold text-purple-800 mb-4">
             📸your memory
           </h1>
           <p className="text-xl text-purple-700 mb-8">
